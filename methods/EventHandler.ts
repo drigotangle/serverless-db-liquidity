44e0b6f360ad2cceece8583e572751f6b73b7c8a
Rodrigo Burgos <drigo@tangleswap.exchange>
2023-04-07 02:35:58 -0300

    deeper price

--- methods/EventHandler.ts
+++ methods/EventHandler.ts
@@ -3,7 +3,9 @@ import {
     getBlockTimestamp,
     getWethPriceAndLiquidity,
     tokenInstance,
-    nftManagerInstance
+    nftManagerInstance,
+    getDeeperPriceAndLiquidity,
+    choosePrice
  } from './'
 
 import { WETH_ADDRESS } from '../constants/index'
@@ -32,6 +34,8 @@ export const eventHandler = async (eventName, tokenId, blockNumber, amount0, amo
         const poolAddress = await getPoolAddress(position.token0, position.token1, fee)
         const account = await NFPM.ownerOf(Number(tokenId))
         const time = await getBlockTimestamp(blockNumber)
+        let price0
+        let price1
         
         amount0 = amount0 / (10 ** decimals0)
         amount1 = amount1 / (10 ** decimals1)
@@ -82,6 +86,14 @@ export const eventHandler = async (eventName, tokenId, blockNumber, amount0, amo
             }
 
             if(![position.token1, position.token2].includes(WETH_ADDRESS)){
+                const wethPrice0 = await getWethPriceAndLiquidity(token0Address, blockNumber)
+                const deeperPrice0 = await getDeeperPriceAndLiquidity(token0Address, blockNumber)
+                const wethPrice1 = await getWethPriceAndLiquidity(token1Address, blockNumber)
+                const deeperPrice1 = await getDeeperPriceAndLiquidity(token1Address, blockNumber)
+
+                price0 = choosePrice(wethPrice0[0].price, deeperPrice0[0].price)
+                price1 = choosePrice(wethPrice1[0].price, deeperPrice1[0].price)
+                
                 const priceAndLiquidity0 = await getWethPriceAndLiquidity(token0Address, blockNumber)
                 const value0 = (priceAndLiquidity0[0]?.price ?? 0) * (amount0) / (10 ** decimals0)
                 const priceAndLiquidity1 = await getWethPriceAndLiquidity(token1Address, blockNumber)
